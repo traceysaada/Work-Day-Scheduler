ba5c19d9ac4b4c9675b0da88c3bf7fa0ec9e5460
Tracey Saada <tracey.saada@outlook.com>
2020-11-20 18:17:09 +0000

    updated html & added btn click in  jquery

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var displayCurrentDateAndTime = document.getElementById("currentDateAndTime");
+  var now = moment();
+  displayCurrentDateAndTime.textContent = now;
+  var presentHour = moment().hours();
 
-    var displayCurrentDateAndTime = document.getElementById("currentDateAndTime");
-    var now = moment();
-    var presentHour = moment().hours();
-    displayCurrentDateAndTime.textContent = now; 
-})
+  function timeCheckPastPresentFuture() {
+    $(".time-block").each(function () {
+      var timeOfCurrentBlock = parseInt($(this).attr("id"));
+
+      if (timeOfCurrentBlock > presentHour) {
+        $(this).addClass("future");
+      } else if (timeOfCurrentBlock === presentHour) {
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+    });
+  }
+  timeCheckPastPresentFuture();
+
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var userInput = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, userInput);
+  });
+
+ var eightTask = localStorage.getItem("8");
+ $("#eightTask").val(eightTask)
+
+
+});
